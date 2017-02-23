cf1c3c723c45cae2203ebb3c59832f4b6e2dabee
Brian Chung <liang.jai@gmail.com>
2017-02-23 14:21:05 -0800

    updating the problem solover

--- problem_solver.rb
+++ problem_solver.rb
@@ -5,7 +5,18 @@ class ProblemSolver
 	def solve_harder_problem
 	end
 
-	def solve+_tough_problem
+	def solve_tough_problem
+	end
+
+	def use_genetic_algorithm
+	   darwin = Darwin.new(problem)
+	   darwin.survive
+	end
+
+	def prolong_solver(description, question)
+	   prolong = Prolong.new
+	   prolong,describe_world(description)
+	   prolong.ask(question)
 	end
 end
 
